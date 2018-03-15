5b590125de95eb446113343be6a29be16896fcda
inu1255 <929909260@qq.com>
2018-03-15 13:35:00 +0800

    0.0.8

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ default keymaps `cmd+. cmd+e`
 
 ## Release Notes
 
+### 0.0.8 eval js formula such as 1+2(it will be replace as 3), surport multi cursor command exec
 ### 0.0.7 add env $file for fullname and $basename for basename of current edit file
 ### 0.0.5 run command current line
 ### 0.0.4 add cloneLine features,you can turn on/off it in configuration
--- extension.js
+++ extension.js
@@ -2,49 +2,74 @@ const vscode = require('vscode');
 const shell = require("child_process")
 const path = require("path")
 
+function exec(editer, selection) {
+    return new Promise((resolve, reject) => {
+        let command = editer.document.getText(selection)
+        if (!command) {
+            let line = editer.document.lineAt(selection.end.line)
+            command = line.text
+            let b = command.length - line.text.trimLeft().length
+            selection = new vscode.Selection(line.lineNumber, b, line.lineNumber, line.range.end.character)
+        }
+        if (!command) return
+        command = command.trim()
+        if (/^\d/.test(command)) {
+            let outstr = eval(command) + "\n"
+            resolve({ selection, outstr })
+        } else {
+            let cmd = shell.exec(command, {
+                cwd: path.dirname(editer.document.fileName),
+                env: Object.assign({}, process.env, {
+                    file: editer.document.fileName,
+                    basename: path.basename(editer.document.fileName)
+                })
+            })
+            let outstr = ""
+            cmd.stdout.on("data", function(data) {
+                outstr += data
+            })
+            cmd.on("exit", function() {
+                resolve({ selection, outstr })
+            })
+            cmd.on("error", function(e) {
+                console.log(command, e)
+                resolve("")
+            })
+        }
+    })
+}
+
 function activate(context) {
     let disposable = vscode.commands.registerCommand('easyShell.run', function() {
         let editer = vscode.window.activeTextEditor
-        let command = editer.document.getText(editer.selection)
-        if (!command) {
-            let line = editer.document.lineAt(editer.selection.end.line)
-            command = line.text
-            let b = command.length - line.text.trimLeft().length
-            editer.selection = new vscode.Selection(line.lineNumber, b, line.lineNumber, line.range.end.character)
-        }
-        if (!command) return
-        let cmd = shell.exec(command, {
-            cwd: path.dirname(editer.document.fileName),
-            env: Object.assign({}, process.env, {
-                file: editer.document.fileName,
-                basename: path.basename(editer.document.fileName)
-            })
-        })
-        var outstr = ""
-        cmd.stdout.on("data", function(data) {
-            outstr += data
-        })
-        cmd.on("exit", function() {
-            let cloneLine = vscode.workspace.getConfiguration().get('easyShell.cloneLine')
-            if (cloneLine && editer.selection.start.line == editer.selection.end.line) {
-                let b = editer.selection.start.character
-                let e = editer.selection.end.character
-                let line_num = editer.selection.start.line;
-                let line = editer.document.lineAt(line_num)
-                let line_end = line.range.end.character
-                let prefix = editer.document.getText(new vscode.Selection(line_num, 0, line_num, b)).trimLeft()
-                let suffix = editer.document.getText(new vscode.Selection(line_num, e, line_num, line_end))
-                outstr = outstr.replace(/([^\n]*)\n/g, prefix + "$1$$1" + suffix + "\n")
-                outstr = outstr.slice(prefix.length, outstr.length - suffix.length - 1)
+        Promise.all(editer.selections.map(x => exec(editer, x))).then(outs => {
+            if (editer.selections.length > 1) {
+                editer.edit(function(edit) {
+                    for (let { selection, outstr } of outs) {
+                        edit.replace(selection, outstr.trim());
+                    }
+                })
             } else {
-                outstr = outstr.replace(/\n/g, "$1\n")
+                let cloneLine = vscode.workspace.getConfiguration().get('easyShell.cloneLine')
+                for (let { selection, outstr } of outs) {
+                    if (cloneLine && selection.start.line == selection.end.line) {
+                        let b = selection.start.character
+                        let e = selection.end.character
+                        let line_num = selection.start.line;
+                        let line = editer.document.lineAt(line_num)
+                        let line_end = line.range.end.character
+                        let prefix = editer.document.getText(new vscode.Selection(line_num, 0, line_num, b)).trimLeft()
+                        let suffix = editer.document.getText(new vscode.Selection(line_num, e, line_num, line_end))
+                        outstr = outstr.replace(/([^\n]*)\n/g, prefix + "$1$$1" + suffix + "\n")
+                        outstr = outstr.slice(prefix.length, outstr.length - suffix.length - 1)
+                    } else {
+                        outstr = outstr.replace(/\n/g, "$1\n")
+                    }
+                    if (outstr[outstr.length - 1] === "\n")
+                        outstr = outstr.slice(0, outstr.length - 1)
+                    editer.insertSnippet(new vscode.SnippetString(outstr), selection)
+                }
             }
-            if (outstr[outstr.length - 1] === "\n")
-                outstr = outstr.slice(0, outstr.length - 1)
-            editer.insertSnippet(new vscode.SnippetString(outstr), editer.selection)
-            // editer.edit(function(edit){
-            //     edit.replace(editer.selection, s);
-            // })
         })
     });
 
